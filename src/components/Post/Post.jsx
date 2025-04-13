594b8132fe72e86aee4306de01c2df144c64edda
filipkk78 <filipkk78@gmail.com>
2025-04-13 17:32:14 +0200

    Add posting comments on posts

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Loading from "../Loading/Loading";
+import styles from "./Post.module.css";
 
 function Post() {
   const { id } = useParams();
   const [post, setPost] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [authorName, setAuthorName] = useState("");
+  const [content, setContent] = useState("");
+  const [pending, setPending] = useState(false);
 
   useEffect(() => {
     fetch(`http://localhost:5000/api/posts/${id}`, { mode: "cors" })
@@ -16,17 +20,64 @@ function Post() {
         }
         return response.json();
       })
-      .then((json) => setPost(json))
+      .then((json) => setPost(json.post))
       .catch((error) => setError(error))
       .finally(() => setLoading(false));
-  }, [id]);
-  console.log(post);
+  }, [post]);
   if (loading) return <Loading></Loading>;
   if (error) return <p>A network error has occured</p>;
 
+  const postId = post.id;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const comment = { authorName, content, postId };
+    setPending(true);
+    fetch("http://localhost:5000/api/comments", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(comment),
+    }).then(() => {
+      setPending(false);
+    });
+  }
+
   return (
     <>
-      <h1>Post {id}</h1>
+      <h1>{post.title}</h1>
+      <p className={styles.content}>{post.content}</p>
+      <h2>Comments</h2>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <label htmlFor="authorName">Name</label>
+        <input
+          type="text"
+          name="authorName"
+          id="authorName"
+          required
+          value={authorName}
+          onChange={(e) => setAuthorName(e.target.value)}
+        />
+        <label htmlFor="content">Content</label>
+        <textarea
+          name="content"
+          id="content"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+
+        {!pending && <button type="submit">Add comment</button>}
+        {pending && <button disabled>Pending...</button>}
+      </form>
+      {post.comments.map((comment) => (
+        <article key={comment.id}>
+          <div className={styles.commentheader}>
+            <span className={styles.commentAuthor}>{comment.authorName}</span> @
+            {new Date(comment.createdAt).toLocaleString()}
+          </div>
+          <p>{comment.content}</p>
+        </article>
+      ))}
     </>
   );
 }
